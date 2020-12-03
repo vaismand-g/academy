5c3c01e150d1600e577bc21f8fad277ddfcdee4a
Damian Vaisman <damian.vaisman@globant.com>
2020-12-03 14:09:24 -0300

    Corrected install_apache.sh

--- install_apache.sh
+++ install_apache.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y httpd.x86_64
-sudo sed -i '/Listen/{s/\([0-9]\+\)/8080/; :a;n; ba}' /etc/httpd/conf/httpd.conf
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
 sudo echo "Hello World from $(hostname -f)" > /var/www/html/index.html
